c6c9094d40f7b7b2c29ba64f36e8434e3eed1147
Black Wood <144987320+Dimo666@users.noreply.github.com>
2025-04-21 21:08:03 +0300

    Валидация Даных

---
+++ hotels.py
@@ -0,0 +1,79 @@
+from fastapi import APIRouter, Body, Query
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/hotels", tags=["Отели"] ) # префикс для маршрутов из данного роутера
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+]
+
+
+@router.get("")
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+# схема данных
+class Hotel(BaseModel):
+    title: str
+    name: str
+
+@router.post("")
+def create_hotel(hotel_data: Hotel):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": hotel_data.title,
+        "name": hotel_data.name
+    })
+    return {"status": "OK"}
+
+
+@router.put("/{hotel_id}")
+def put_hotel(hotel_id: int,hotel_data: Hotel):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = hotel_data.title
+            hotel["name"] = hotel_data.name
+            return {"status": "OK"}
+    return {"error": "Hotel not found"}
+
+
+@router.patch(
+    "/{hotel_id}",
+    summary="Частичное обновление данных об отеле",
+    description="Тут мы частично обновляем данные об отеле: можно отправить"
+    "name, а можно title"
+)
+def patch_hotel(
+    hotel_id: int,
+    name: str | None = Body(None),
+    title: str | None = Body(None)
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            if title is not None:
+                hotel["title"] = title
+            if name is not None:
+                hotel["name"] = name
+            return {"status": "OK"}
+    return {"error": "Hotel not found"}
+
+
+@router.delete("/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
