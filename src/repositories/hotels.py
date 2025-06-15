3f74be9d142e621291f6ac70e2c70520be20ccfc
Black Wood <144987320+Dimo666@users.noreply.github.com>
2025-06-16 00:46:12 +0300

    Задание №14: Вернуть пагинацию и фильтрацию в получение отелей

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,51 +1,44 @@
 from datetime import date
-
+from sqlalchemy import select, func
 from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
-from sqlalchemy import select, func
-
 from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
-
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
-            self,
-            location,
-            title,
-            limit,
-            offset,
-    ) -> list[Hotel]:
-        query = select(HotelsOrm)
-        if location:
-            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
-            # query = query.filter(func.lower(HotelsOrm.location).like(f"%{location.strip().lower()}%"))
-        if title:
-            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
-            # query = query.filter(func.lower(HotelsOrm.title).like(f"%{title.strip().lower()}%"))
-        query = (
-            query
-            .limit(limit)
-            .offset(offset)
-        )
-        print(query.compile(compile_kwargs={"literal_binds": True}))
-        result = await self.session.execute(query)
-
-        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
-
     async def get_filtered_by_time(
             self,
             date_from: date,
             date_to: date,
+            location: str | None = None,
+            title: str | None = None,
+            limit: int = 10,
+            offset: int = 0
     ):
+        # Получаем ID свободных комнат
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+
+        # Получаем ID отелей, у которых есть эти свободные комнаты
         hotels_ids_to_get = (
             select(RoomsOrm.hotel_id)
             .select_from(RoomsOrm)
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
         )
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
+
+        # Основной запрос к таблице отелей
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
+
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+        if title:
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+
+        query = query.limit(limit).offset(offset)
+
+        result = await self.session.execute(query)
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+
